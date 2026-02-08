1b5ce4c8176c5b7d700d97c050ded27ae739a6e3
AleZane29 <alessio.zanella29@gmail.com>
2026-02-08 12:15:12 +0100

    Eliminaizione commento

--- frontend/src/components/utils/languageImports.ts
+++ frontend/src/components/utils/languageImports.ts
@@ -1,28 +1,26 @@
-// src/utils/languageImports.ts
-
 // --- WEB ---
+import 'codemirror/mode/css/css'; // CSS
+import 'codemirror/mode/htmlmixed/htmlmixed'; // HTML
 import 'codemirror/mode/javascript/javascript'; // JS, Node
-import 'codemirror/mode/jsx/jsx';               // React
-import 'codemirror/mode/css/css';               // CSS
-import 'codemirror/mode/sass/sass';             // SCSS/Sass
-import 'codemirror/mode/htmlmixed/htmlmixed';   // HTML
-import 'codemirror/mode/xml/xml';               // XML
+import 'codemirror/mode/jsx/jsx'; // React
+import 'codemirror/mode/sass/sass'; // SCSS/Sass
+import 'codemirror/mode/xml/xml'; // XML
 
 // --- BACKEND & SOFTWARE ---
-import 'codemirror/mode/python/python';         // Python
-import 'codemirror/mode/sql/sql';               // SQL
-import 'codemirror/mode/go/go';                 // Go
-import 'codemirror/mode/rust/rust';             // Rust
-import 'codemirror/mode/clike/clike';           // C, C++, C#, Java, Kotlin, Scala
-import 'codemirror/mode/php/php';               // PHP
+import 'codemirror/mode/clike/clike'; // C, C++, C#, Java, Kotlin, Scala
+import 'codemirror/mode/go/go'; // Go
+import 'codemirror/mode/php/php'; // PHP
+import 'codemirror/mode/python/python'; // Python
+import 'codemirror/mode/rust/rust'; // Rust
+import 'codemirror/mode/sql/sql'; // SQL
 
 // --- SCRIPTING & CONFIG ---
-import 'codemirror/mode/shell/shell';           // Bash / Terminale
-import 'codemirror/mode/powershell/powershell'; // PowerShell
-import 'codemirror/mode/yaml/yaml';             // YAML
 import 'codemirror/mode/dockerfile/dockerfile'; // Docker
-import 'codemirror/mode/nginx/nginx';           // Nginx
+import 'codemirror/mode/nginx/nginx'; // Nginx
+import 'codemirror/mode/powershell/powershell'; // PowerShell
 import 'codemirror/mode/properties/properties'; // .env / .properties
+import 'codemirror/mode/shell/shell'; // Bash / Terminale
+import 'codemirror/mode/yaml/yaml'; // YAML
 
 // --- DATA ---
-import 'codemirror/mode/markdown/markdown';     // Markdown stesso
+import 'codemirror/mode/markdown/markdown'; // Markdown stesso
