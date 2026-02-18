530fc91819c96f3c0efb7113f30f8040fffd5e81
Steva0 <michele.stevanin@gmail.com>
2026-02-18 19:40:58 +0100

    Aggiunto docker con aggiornamento automatico al cambiamento del codice

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/ProofOfConcept/',
+  server: {
+    watch: {
+      usePolling: true, 
+    },
+    host: true,
+    strictPort: true,
+    port: 5173,
+  }
 })
